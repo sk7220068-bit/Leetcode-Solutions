41f796c5dc843e1287cc989072de86de1aebd863
Shivam Kumar <sk7220068@gmail.com>
2025-11-20 08:59:23 +0530

    Multipling no

---
+++ 2154. Keep Multiplying Found Values by Two/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(original == nums[i]){
+                original*=2;
+            }
+        }
+        return original;
+    }
+}
